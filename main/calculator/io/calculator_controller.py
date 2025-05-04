ea861689c1a52a16d4c5eeef1fb2aaacba9a9d0f
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-05-04 17:25:12 +0100

    calculator python backend

---
+++ main/calculator/io/calculator_controller.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from main.calculator.services.calculator import calculate
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/calculate", methods=['POST'])
+def evaluate_expression():
+    data:dict = request.get_json(force=True)
+    # expression = data["expression"]
+    expression = data.get('expression',"").strip()
+    if expression == "":
+        return jsonify({"error": "expression cannot be empty"})
+
+    # print(expression)
+    try:
+        result = calculate(expression)
+        return jsonify({"result": str(result)}), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        return jsonify({"error": "Calculation error"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
