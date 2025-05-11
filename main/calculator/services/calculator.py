69777288858863f30b84b392038c80129524e5ef
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-05-11 07:07:01 +0100

    created the _evaluate_operation function

--- main/calculator/services/calculator.py
+++ main/calculator/services/calculator.py
@@ -1,83 +1,93 @@
+def replace_math_symbol_with_language_operator(expression):
+    expression = expression.replace(" ", "")
+    expression = expression.replace("x", "*")
+    expression = expression.replace("÷", "/")
+    expression = expression.replace("^", "**")  # consider where it is placed
+    expression = expression.replace("mod", "%")
+    return expression
+
+
 def generate_expression_list(expression: str) -> list:
     # handle polarity of number, at the beginning, the last non-operator-operator eval("----2")
 
-    expression = expression.replace(" ", "")
-    expression = expression.replace("x", "*")
+    expression = replace_math_symbol_with_language_operator(expression)
 
     new_list = []
     temp = ""
+
     for character in expression:
-        if character not in ["%", "*", "/", "+", "-"]:
+        if character in {"%", "*", "/", "+", "-"}:
+            new_list.extend([temp, character])
+            temp = ""
+        else:
             temp += character
-            continue
-        elif character == "*":
-            new_list.extend([temp, "*"])
-        elif character == "/":
-            new_list.extend([temp, "/"])
-        elif character == "%":
-            new_list.extend([temp, "%"])
-        elif character == "+":
-            new_list.extend([temp, "+"])
-        elif character == "-":
-            new_list.extend([temp, "-"])
-        temp = ""
     new_list.append(temp)
     return new_list
 
 
-def resolved_expression(expression_list, index, operation) -> list:
+def __resolved_expression(expression_list, index, operation) -> list:
     new_element = float(0)
+
+    a = float(expression_list[index - 1])
+    b = float(expression_list[index + 1])
+
     if operation == "*":
-        new_element = float(expression_list[index - 1]) * float(expression_list[index + 1])
+        new_element = a * b
     elif operation == "/":
-        new_element = float(expression_list[index - 1]) / float(expression_list[index + 1])
+        new_element = a / b
     elif operation == "%":
-        new_element = float(expression_list[index - 1]) % float(expression_list[index + 1])
+        new_element = a % b
     elif operation == "+":
-        new_element = float(expression_list[index - 1]) + float(expression_list[index + 1])
+        new_element = a + b
     elif operation == "-":
-        new_element = float(expression_list[index - 1]) - float(expression_list[index + 1])
+        new_element = a - b
 
     expression_list = expression_list[:index - 1] + [str(new_element)] + expression_list[index + 2:]
     return expression_list
 
-
-def evaluate_multiplications(expression_list: list) -> list:
+def _evaluate_operation(expression_list: list[str], operator: str) -> list[str]:
     for index, element in enumerate(expression_list):
-        if element == "*" and len(expression_list) > 1:
-            expression_list = resolved_expression(expression_list=expression_list, index=index, operation="*")
-            return evaluate_multiplications(expression_list)
+        if element == operator and len(expression_list) > 1:
+            expression_list = __resolved_expression(expression_list=expression_list, index=index, operation=operator)
+            return _evaluate_operation(expression_list, operator)
     return expression_list
+
+def evaluate_multiplications(expression_list: list[str]) -> list[str]:
+    return _evaluate_operation(expression_list, "*")
 
 
 def evaluate_divisions(expression_list) -> list:
+    operator = "/"
     for index, element in enumerate(expression_list):
-        if element == "/" and len(expression_list) > 1:
-            expression_list = resolved_expression(expression_list=expression_list, index=index, operation="/")
+        if element == operator and len(expression_list) > 1:
+            expression_list = __resolved_expression(expression_list=expression_list, index=index, operation=operator)
             return evaluate_divisions(expression_list)
     return expression_list
 
 
 def evaluate_remainders(expression_list: list) -> list:
+    operator = "%"
     for index, element in enumerate(expression_list):
-        if element == "%" and len(expression_list) > 1:
-            expression_list = resolved_expression(expression_list=expression_list, index=index, operation="%")
+        if element == operator and len(expression_list) > 1:
+            expression_list = __resolved_expression(expression_list=expression_list, index=index, operation=operator)
             return evaluate_remainders(expression_list)
     return expression_list
 
 
 def evaluate_additions(expression_list: list) -> list:
+    operator = "+"
     for index, element in enumerate(expression_list):
-        if element == "+" and len(expression_list) > 1:
-            expression_list = resolved_expression(expression_list=expression_list, index=index, operation="+")
+        if element == operator and len(expression_list) > 1:
+            expression_list = __resolved_expression(expression_list=expression_list, index=index, operation=operator)
             return evaluate_additions(expression_list)
     return expression_list
 
 
 def evaluate_subtractions(expression_list: list) -> list:
+    operator = "-"
     for index, element in enumerate(expression_list):
-        if element == "-" and len(expression_list) > 1:
-            expression_list = resolved_expression(expression_list=expression_list, index=index, operation="-")
+        if element == operator and len(expression_list) > 1:
+            expression_list = __resolved_expression(expression_list=expression_list, index=index, operation=operator)
             return evaluate_subtractions(expression_list)
     return expression_list
 
