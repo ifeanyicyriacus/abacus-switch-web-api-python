58c4ffceeb0ebf868993a115e9d7608779d55e34
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-05-25 00:34:43 +0100

    pre render deployment
    -handling cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,13 @@ from flask_cors import CORS
 from app.calculator.calculator_controller import calculate_controller_bp
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=[
+    "https://abacus-switch-web-app-react-frontend.vercel.app/",
+    "https://52.41.36.82",
+    "https://54.191.253.12",
+    "https://44.226.122.3",
+    "http://localhost:5173"
+])
 
 from app.calculator import calculator_controller
 app.register_blueprint(calculate_controller_bp)
